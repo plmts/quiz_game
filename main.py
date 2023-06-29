6449f67d675f556b77c83cc3990cdfcd1403d156
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-06-29 18:28:00 -0300

    final commit. project is over.

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
